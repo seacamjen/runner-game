76f87c106d71c72ab55729bad645e98e8758da7f
Cameron Jensen <jensenseanc@gmail.com>
2017-05-16 10:13:10 -0700

    add movement by arrowkeys

--- src/assets/runner.js
+++ src/assets/runner.js
@@ -3,6 +3,15 @@ var character;
 var bitmap;
 var hero;
 
+var KEYCODE_UP = 38;
+var KEYCODE_LEFT = 37;
+var KEYCODE_RIGHT = 39;
+var KEYCODE_DOWN = 40;
+var leftArrow = false;
+var rightArrow = false;
+var upArrow = false;
+var downArrow = false;
+
 function init() {
   stage = new createjs.Stage("demoCanvas");
 
@@ -26,16 +35,19 @@ function init() {
           // .to({alpha: 1, y: 100}, 500, createjs.Ease.getPowInOut(2))
           // .to({x: 100}, 800, createjs.Ease.getPowInOut(2));
 
-  // startGame();
   createjs.Ticker.setFPS(80);
   createjs.Ticker.addEventListener("tick", tick);
   // stage.addEventListener("click", fly);
-  stage.addEventListener("click", fly);
+  stage.addEventListener("click", jump);
 
+  window.onkeyup = keyUpHandler;
+  window.onkeydown = keyDownHandler;
+  console.log(leftArrow);
 }
 
 function tick() {
-  bitmap.x += 1;
+  // bitmap.x += 1;
+  move();
   stage.update();
   if (bitmap.x > 800) {
     bitmap.x = 0
@@ -48,24 +60,38 @@ function tick() {
 //   stage.update();
 // }
 
-function fly() {
+function jump() {
   createjs.Tween.get(bitmap, {loop: false})
     .to({y: bitmap.y}, 500, createjs.Ease.getPowInOut(3))
     .to({y: bitmap.y -=100}, 1000, createjs.Ease.getPowInOut(3))
     .to({y: bitmap.y +=100}, 1000, createjs.Ease.getPowInOut(3))
 
 }
-// function startGame() {
-//   hero = new Hero(character);
-//   stage.addChild(hero);
-//   hero.reset();
-//
-//   createjs.Ticker.setFPS(30);
-//   createjs.Ticker.addEventListener("tick", stage);
-// }
-//
-// function tick() {
-//   hero.tick();
-//   console.log(hero.tick());
-//   stage.update();
-// }
+
+function keyDownHandler(e) {
+  switch(e.keyCode) {
+    case KEYCODE_RIGHT: rightArrow = true; break;
+    case KEYCODE_LEFT: leftArrow = true; break;
+    case KEYCODE_UP: upArrow = true; break;
+    case KEYCODE_DOWN: downArrow = true; break;
+    console.log(leftArrow);
+  }
+}
+
+function keyUpHandler(e) {
+  switch(e.keyCode) {
+    case KEYCODE_RIGHT: rightArrow = false; break;
+    case KEYCODE_LEFT: leftArrow = false; break;
+    case KEYCODE_UP: upArrow = false; break;
+    case KEYCODE_DOWN: downArrow = false; break;
+
+  }
+}
+
+function move() {
+  if(rightArrow) bitmap.x -= 5;
+  if(leftArrow) bitmap.x += 5;
+  if(upArrow) bitmap.y -= 5;
+  if(downArrow) bitmap.y += 5;
+  stage.update();
+}
