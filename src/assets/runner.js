9eef0af72a904053bd88eb0baaa6719f815f184e
Cameron Jensen <jensenseanc@gmail.com>
2017-05-16 13:18:49 -0700

    add collision between projects

--- src/assets/runner.js
+++ src/assets/runner.js
@@ -1,7 +1,12 @@
 var stage;
 var character;
 var bitmap;
-var hero;
+var bitWidth = 107;
+var bitHeight = 200;
+var splatImg;
+var splat;
+var splatWidth = 50;
+var splatHeight = 47;
 
 var KEYCODE_UP = 38;
 var KEYCODE_LEFT = 37;
@@ -20,32 +25,26 @@ function init() {
   bitmap = new createjs.Bitmap(character);
   bitmap.x = 0;
   bitmap.y = 350;
-  stage.addChild(bitmap);
+
+  splatImg = new Image();
+  splatImg.src = "assets/stickeysplat.jpg";
+  splat = new createjs.Bitmap(splatImg);
+  splat.x = 800;
+  splat.y = 500;
+
+  stage.addChild(splat, bitmap);
   stage.update();
-
-  bitmap.x = bitmap.x + 3;
-  if (bitmap.x > stage.canvas.width){
-    bitmap.x = 0;
-  }
-
-  // createjs.Tween.get(bitmap, {loop: true})
-  //         .to({x: 300, y: 200}, 1000, createjs.Ease.getPowInOut(2))
-  //         .to({alpha: 0, x: 500, y: 75}, 500, createjs.Ease.getPowInOut(2))
-          // .to({alpha: 0, y: 125}, 100)
-          // .to({alpha: 1, y: 100}, 500, createjs.Ease.getPowInOut(2))
-          // .to({x: 100}, 800, createjs.Ease.getPowInOut(2));
 
   createjs.Ticker.setFPS(80);
   createjs.Ticker.addEventListener("tick", tick);
-  // stage.addEventListener("click", fly);
-  // stage.addEventListener("click", jump);
 
   window.onkeyup = keyUpHandler;
   window.onkeydown = keyDownHandler;
-  console.log(leftArrow);
+
 }
 
 function tick() {
+  splat.x -= 1;
   bitmap.x += 1;
   move();
   stage.update();
@@ -60,6 +59,20 @@ function tick() {
   if (bitmap.y > 600) {
     bitmap.x = 0;
     bitmap.y = 350;
+  }
+  if (splat.x > 1000) {
+    splat.x = 800;
+    splat.y = 500;
+  }
+  if (splat.x < -20) {
+    splat.x = 800;
+    splat.y = 500;
+  }
+
+  if (splat.x < bitmap.x + bitWidth && splat.x + splatWidth > bitmap.x && splat.y < bitmap.y + bitHeight && splat.y + splatHeight > bitmap.y) {
+    splat.x = 800;
+    splat.y = 500;
+    stage.update();
   }
 }
 
